8e7aaeb1c638e90b581512fad2456027ff39b2c4
Elad Tsfany <eladtsfany@gmail.com>
2023-11-22 19:28:15 +0200

    Add logic to the game, can play in console without win condition for now

---
+++ src/javascript.js
@@ -0,0 +1,98 @@
+// Factory function -> IIFE (Immediately Invoked Function Expression) - Runs when defined..
+
+//This is NOT the case
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => board;
+    //logging the board with symbols only
+    const printBoard = () => {
+        const boardSymbols = board.map(row => row.map(cell => cell.getSymbol()));
+        console.log(boardSymbols);
+    };
+
+    const markSymbol = (player, row, column) => {
+        if (board[row][column].getSymbol() != 0) {
+            console.log(`Selected cell [${row}, ${column}] is already marked..`);
+            return;
+        }
+        board[row][column].addSymbol(player);
+    }
+
+    return {
+        getBoard,
+        printBoard,
+        markSymbol
+    };
+};
+
+function Cell() {
+    // 0 = empty | 1 = player1 (X) | 2 = player2 (O)
+    let value = 0;
+
+    const addSymbol = (player) => { value = player };
+    const getSymbol = () => value;
+
+    return { addSymbol, getSymbol };
+}
+
+function GameController(playerOneName, playerTwoName) {
+    const board = Gameboard();
+
+    const players = [
+        {
+            name: playerOneName,
+            token: 1
+        },
+        {
+            name: playerTwoName,
+            token: 2
+        }
+    ];
+
+    let activePlayer = players[0];
+    const getActivePlayer = () => activePlayer;
+
+    const switchPlayerTurn = () => {
+        (getActivePlayer() === players[0]) ? activePlayer = players[1] : activePlayer = players[0];
+    };
+
+    const printRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+    };
+
+    const playRound = (row, column) => {
+        if (board.getBoard()[row][column].getSymbol() != 0) {
+            console.log(`Selected cell [${row}, ${column}] is already marked..`);
+            console.log(`Still ${getActivePlayer().name}'s turn.`);
+            return;
+        }
+        // Add corresponding symbol to cell
+        console.log(`${getActivePlayer().name} has marked cell [${row}, ${column}].`);
+        board.getBoard()[row][column].addSymbol(getActivePlayer().token);
+
+        /*  Winner checks..
+              some code.. */
+
+        // Progress game
+        switchPlayerTurn();
+        printRound();
+    };
+
+    //First round print:
+    printRound();
+
+    return { playRound };
+}
+
+const game = GameController('Player1', 'Player2');
